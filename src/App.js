adf3b7b552990d0debb4de811b8569201500938c
Kate Dubitski <kate.dubitski@gmail.com>
2020-06-09 15:49:37 -0400

    Create CoinToss component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import CoinToss from './CoinToss';
+
 
 function App() {
   return (
     <div className="App">
+      <CoinToss/>
     </div>
   );
 }
---
+++ src/CoinToss.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class CoinToss extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>Coin Tossing action here!</h1>
+      </div>
+    )
+  }
+}
+
+export default CoinToss;
