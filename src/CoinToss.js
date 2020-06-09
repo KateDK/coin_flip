b621d98be619d5facc9730d84102f6a60ede2a5d
Kate Dubitski <kate.dubitski@gmail.com>
2020-06-09 16:01:53 -0400

    Add initial state

--- src/CoinToss.js
+++ src/CoinToss.js
@@ -6,6 +6,12 @@ class CoinToss extends React.Component{
     heads:'https://tinyurl.com/react-coin-heads-jpg',
     tails:'https://tinyurl.com/react-coin-tails-jpg',
   }
+
+  state ={
+    headsDrawn: 0,
+    tailsDrawn: 0,
+    currentDraw: 'heads',
+  }
   render(){
     return (
       <div>
