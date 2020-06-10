695485b2597b688690b59ac1a1e48ea944a5f187
Kate Dubitski <kate.dubitski@gmail.com>
2020-06-10 17:16:05 -0400

    render ColorBox div

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,5 +1,13 @@
 import React from 'react';
 class ColorBox extends React.Component{
+  render(){
+    const {color} = this.state;
+    return(
+      <div className='ColorBox' style={divStyle} onClick={this.genColor}>
+        ColorBox
+      </div>
+    )
+  }
 }
 
 export default ColorBox;
