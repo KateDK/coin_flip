d873a7b7424a2e913bd09b7997d74f9aa6a39a64
Kate Dubitski <kate.dubitski@gmail.com>
2020-06-10 17:11:08 -0400

    Create ColorGrid component & import to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import CoinToss from './CoinToss';
+import ColorGrid from './ColorGrid'
 
 
 function App() {
   return (
     <div className="App">
       <CoinToss/>
+      <ColorGrid/>
     </div>
   );
 }
---
+++ src/ColorGrid.js
@@ -0,0 +1,5 @@
+import React from 'react';
+class ColorGrid extends React.Component{
+}
+
+export default ColorGrid;
