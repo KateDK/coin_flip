c0ec870cdd1d649d2555865224b88823c9921991
Kate Dubitski <kate.dubitski@gmail.com>
2020-06-10 17:13:24 -0400

    add grid rendering rendering based on staticProps

--- src/ColorGrid.js
+++ src/ColorGrid.js
@@ -1,5 +1,16 @@
 import React from 'react';
 class ColorGrid extends React.Component{
+  static defaultProps = {
+    nColor:16,
+  }
+  render(){
+    const {nColor} = this.props;
+    return(
+      <div className='ColorGrid'>
+        {[...Array(nColor)].map((e,i) => <ColorBox key={i}/>)}
+      </div>
+    )
+  }
 }
 
 export default ColorGrid;
