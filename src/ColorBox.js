fed200f5a945f5df416ccae58e8326aec5620e7d
Kate Dubitski <kate.dubitski@gmail.com>
2020-06-10 17:17:01 -0400

    Add divStyle for inline styling

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 class ColorBox extends React.Component{
 
   state={
@@ -6,8 +7,14 @@ class ColorBox extends React.Component{
   }
 
   genColor = () => this.setState({color:Math.floor(Math.random()*16777215).toString(16)});
+
   render(){
     const {color} = this.state;
+    const divStyle = {
+      backgroundColor: `#${color}`,
+      width: 250,
+      height: 100,
+    };
     return(
       <div className='ColorBox' style={divStyle} onClick={this.genColor}>
         ColorBox
