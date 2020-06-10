6122e711a4725e73545e4622609ff9ce3c2b2ec0
Kate Dubitski <kate.dubitski@gmail.com>
2020-06-10 17:16:33 -0400

    Add state and click handler function

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,5 +1,11 @@
 import React from 'react';
 class ColorBox extends React.Component{
+
+  state={
+    color: Math.floor(Math.random()*16777215).toString(16),
+  }
+
+  genColor = () => this.setState({color:Math.floor(Math.random()*16777215).toString(16)});
   render(){
     const {color} = this.state;
     return(
