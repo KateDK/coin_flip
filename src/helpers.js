a2229eb33bacb4bb58937ece08aa32068aba340b
Kate Dubitski <kate.dubitski@gmail.com>
2020-07-13 17:10:32 -0400

    Refactor helpers for cleaner look

--- src/helpers.js
+++ src/helpers.js
@@ -1,10 +1,8 @@
 const randomChoice = (arr) =>{
-  const randomChoice = Math.floor(Math.random()*arr.length);
-  return arr[randomChoice];
+  return arr[Math.floor(Math.random()*arr.length)];
 }
 
 const makeRandomHSLColor = () => {
-
   return `hsl(${Math.floor(Math.random() * (284 - 262)) + 262},80%,55%)`;
 }
 
